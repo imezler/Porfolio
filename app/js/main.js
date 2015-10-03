2bc474b39ef2262d62924408a88d6f6236146441
Ilya Metsler <imetsler@gmail.com>
2015-10-04 00:27:17 +0600

    modal form added with jquery

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,20 @@
-$(function(){
-	console.log("Функционал main.js успешно загружен")
+$(document).ready(function() {
+    $('#innerlink').click( function(event) {
+        event.preventDefault();
+        $('.addprojpop').fadeIn(400,
+            function(){
+            	$('.project')
+            		.css('display', 'block')
+            		.animate({opacity:1, top: '15%'}, 200);
+        });
+    });
+    /* Зaкрытие мoдaльнoгo oкнa, тут делaем тo же сaмoе нo в oбрaтнoм пoрядке */
+    $('btnclose, .addprojpop').click( function(){
+    	$('.project')
+    		.animate({opacity: 0, top: '10%'}, 200,
+    			function(){
+    				$(this).css('display', 'none');
+    				$('.addprojpop').fadeOut(400);
+    			});
+    });
 });
